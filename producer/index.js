fd9117856d6c8f53b792fceae4f0dd698b3da09e
thxwelchs <thxwelchs@gmail.com>
2020-04-18 05:52:25 +0900

    producer code 수정

--- producer/index.js
+++ producer/index.js
@@ -30,14 +30,14 @@ async function logFileRead(path) {
     return
   }
 
-  const logs = result.split('\n').map( log => { 
+  const logs = result.split('\n').filter(log => log).map( log => { 
     const hashKey = 'xxxxxxx'.replace(/[xy]/g, function(c) {
         const r = Math.random()*16|0
         const v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
     });
     return {
-      Data: Buffer.from(log.replace(/\r/g, '')),
+      Data: Buffer.from(log),
       PartitionKey: hashKey,
     }
   })
